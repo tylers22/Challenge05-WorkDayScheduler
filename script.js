86bf3d53b57f13e46eed40975ab9225cf64d31f9
tylers22 <tylersharp1@gmail.com>
2021-09-22 19:03:30 -0700

    added event listener

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-var campgrounds = document.getElementById("currentDay"); 
+var button = document.getElementById("button");
+var currentDay = document.getElementById("currentDay"); 
+console.log(currentDay);
 
-moment().format('MMMM Do YYYY, h:mm:ss a');
+
+//Moment Current Time
+currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+// Store
+localStorage.setItem("taskname09", "");
+localStorage.setItem("taskname10", "");
+localStorage.setItem("taskname11", "");
+localStorage.setItem("taskname12", "");
+localStorage.setItem("taskname13", "");
+localStorage.setItem("taskname14", "");
+localStorage.setItem("taskname15", "");
+localStorage.setItem("taskname16", "");
+
+// Retrieve
+document.getElementById("result").innerHTML = localStorage.getItem("taskname09");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname10");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname11");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname12");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname13");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname14");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname15");
+document.getElementById("result").innerHTML = localStorage.getItem("taskname16");
+
+
+
+console.log("asdjhsadg")
+button.addEventListener("click", function(event){
+   //alert(event.target);
+   console.log("clicked")
+});
