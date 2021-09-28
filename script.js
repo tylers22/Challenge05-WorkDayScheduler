a0b158aa3fb7386a89591ead32bc99e66b53bdaf
tylers22 <tylersharp1@gmail.com>
2021-09-27 18:40:49 -0700

    still working the issue

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
+var button6 = document.getElementById("button6");
+var button7 = document.getElementById("button7");
+var button8 = document.getElementById("button8");
 var currentDay = document.getElementById("currentDay"); 
 console.log(currentDay);
 console.log(button1, button2); 
@@ -7,21 +13,16 @@ console.log(button1, button2);
 //Moment Current Time
 currentDay.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-
-// Store
-
-
-
 // Retrieve from LS and set the value for the input fields 
 
 document.getElementById("taskname09").value = localStorage.getItem("taskname09");
 document.getElementById("taskname10").value = localStorage.getItem("taskname10");
-/* document.getElementById("result").innerHTML = localStorage.getItem("taskname11");
-document.getElementById("result").innerHTML = localStorage.getItem("taskname12");
-document.getElementById("result").innerHTML = localStorage.getItem("taskname13");
-document.getElementById("result").innerHTML = localStorage.getItem("taskname14");
-document.getElementById("result").innerHTML = localStorage.getItem("taskname15");
-document.getElementById("result").innerHTML = localStorage.getItem("taskname16");
+document.getElementById("taskname11").value = localStorage.getItem("taskname11");
+/* document.getElementById("taskname12").value = localStorage.getItem("taskname12");
+document.getElementById("taskname13").value = localStorage.getItem("taskname13");
+document.getElementById("taskname14").value = localStorage.getItem("taskname14");
+document.getElementById("taskname15").value = localStorage.getItem("taskname15");
+document.getElementById("taskname16").value = localStorage.getItem("taskname16");
 */ 
 
 function savetoLocalStorage(){
